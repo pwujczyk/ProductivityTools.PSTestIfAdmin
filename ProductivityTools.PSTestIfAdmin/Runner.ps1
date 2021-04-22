52faab59738e5a84952ce7cea296575a7bc73789
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-04-22 15:04:03 +0200

    rename

--- ProductivityTools.PSTestIfAdmin/Runner.ps1
+++
@@ -1,4 +0,0 @@
-clear
-cd $PSScriptRoot
-Import-Module .\ProductivityTools.PSTestIfAdmin.psm1 -Force
-Test-IfAdmin -Verbose
---
+++ ProductivityTools.TestIfAdmin/Runner.ps1
@@ -0,0 +1,4 @@
+clear
+cd $PSScriptRoot
+Import-Module .\ProductivityTools.TestIfAdmin.psm1 -Force
+Test-IfAdmin -Verbose
